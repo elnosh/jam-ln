ef82c81aa8a25848a3f4d48efd2a68df3f3c548e
elnosh <elnosh@pm.me>
2025-06-25 16:40:47 -0500

    wip: build reputation

--- ln-simln-jamming/src/attacks/utils.rs
+++ ln-simln-jamming/src/attacks/utils.rs
@@ -1,12 +1,22 @@
+use std::sync::Arc;
+
 use bitcoin::secp256k1::PublicKey;
 use lightning::{
-    ln::msgs::LightningError,
+    ln::{msgs::LightningError, PaymentHash},
     routing::{
         gossip::NetworkGraph,
         router::{build_route_from_hops, PaymentParameters, Route, RouteParameters},
     },
 };
-use simln_lib::sim_node::WrappedLog;
+use rand::Rng;
+use simln_lib::{
+    clock::Clock,
+    sim_node::{SimGraph, SimNode, WrappedLog},
+    LightningNode,
+};
+use tokio::sync::Mutex;
+
+use crate::{clock::InstantClock, reputation_interceptor::ReputationMonitor, BoxError};
 
 pub fn build_custom_route(
     sender: &PublicKey,
@@ -31,4 +41,112 @@ pub fn build_custom_route(
         &WrappedLog {},
         &[0; 32],
     )
+}
+
+/// Helper to build reputation for a specific incoming channel (target_channel)
+/// by sending a batch of payments. The `routes` param is a map from sender node public keys to their
+/// respective `Route`. This allows for building reputation from multiple senders e.g:
+///
+/// - Route 1: A → B → C
+/// - Route 2: D → B → C
+///
+/// In both cases, the channel B <-> C is the outgoing channel for the last hop, and both senders
+/// (A and D) will send payments through this channel.
+///
+/// The `target_channel` is channel for which reputation is being built. Here we should monitor the
+/// outgoing channel reputation against the target_channel revenue.
+pub async fn build_reputation<C: Clock + InstantClock, R: ReputationMonitor>(
+    attacker_node: Arc<Mutex<SimNode<SimGraph>>>,
+    hops: &[PublicKey],
+    network_graph: &NetworkGraph<&WrappedLog>,
+    htlcs: Vec<u64>,
+    target_channel: (PublicKey, u64),
+    reputation_monitor: Arc<R>,
+    clock: Arc<C>,
+) -> Result<u64, BoxError> {
+    let mut total_fees_paid = 0;
+
+    let mut attacker = attacker_node.lock().await;
+    let current_target_revenue = reputation_monitor
+        .list_channels(target_channel.0, InstantClock::now(&*clock))
+        .await?
+        .get(&target_channel.1)
+        .ok_or("target channel not found")?
+        .bidirectional_revenue as u64;
+
+    println!("target revenue before payment {}", current_target_revenue);
+
+    let attacker_pubkey = attacker.get_info().pubkey;
+    let mut route =
+        build_custom_route(&attacker_pubkey, 1000, hops, network_graph).map_err(|e| e.err)?;
+
+    let last_hop_channel = route.paths[0].hops.last().unwrap().short_channel_id;
+    let current_attacker_reputation = reputation_monitor
+        .list_channels(target_channel.0, InstantClock::now(&*clock))
+        .await?
+        .get(&last_hop_channel)
+        .unwrap()
+        .outgoing_reputation as u64;
+
+    println!(
+        "attacker reputation before payment {}",
+        current_attacker_reputation
+    );
+
+    let htlc_amounts: u64 = htlcs.iter().sum();
+    for path in route.paths.iter_mut() {
+        total_fees_paid += path.hops.iter().map(|hop| hop.fee_msat).sum::<u64>();
+
+        let target_hop = match path
+            .hops
+            .iter_mut()
+            .find(|hop| hop.pubkey == target_channel.0)
+        {
+            Some(hop) => hop,
+            None => continue,
+        };
+
+        target_hop.fee_msat = current_target_revenue - current_attacker_reputation + htlc_amounts;
+    }
+
+    let payment_hash = PaymentHash(get_random_bytes());
+    if let Err(e) = attacker
+        .send_to_route(route.clone(), payment_hash, None)
+        .await
+    {
+        return Err(e.to_string().into());
+    }
+
+    let target_revenue = reputation_monitor
+        .list_channels(target_channel.0, InstantClock::now(&*clock))
+        .await?
+        .get(&target_channel.1)
+        .ok_or("target channel not found")?
+        .bidirectional_revenue;
+
+    println!("target revenue after payment {}", target_revenue);
+
+    let last_hop_channel = route.paths[0].hops.last().unwrap().short_channel_id;
+    let attacker_reputation = reputation_monitor
+        .list_channels(target_channel.0, InstantClock::now(&*clock))
+        .await?
+        .get(&last_hop_channel)
+        .unwrap()
+        .outgoing_reputation;
+
+    println!("attacker reputation after payment {}", attacker_reputation);
+
+    if attacker_reputation >= target_revenue {
+        println!("FINISHED BUILDING REPUTATION!");
+        Ok(total_fees_paid)
+    } else {
+        Err("could not build reputation".into())
+    }
+}
+
+pub fn get_random_bytes() -> [u8; 32] {
+    let mut rng = rand::rng();
+    let mut bytes = [0u8; 32];
+    rng.fill(&mut bytes[..]);
+    bytes
 }
