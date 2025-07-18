d17dd44e6e2847548566a47abc60a77bef4e7add
elnosh <elnosh@pm.me>
2025-07-18 09:19:55 -0500

    f - comments and use track_payment

--- ln-simln-jamming/src/attacks/utils.rs
+++ ln-simln-jamming/src/attacks/utils.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, thread, time::Duration};
+use std::sync::Arc;
 
 use bitcoin::secp256k1::PublicKey;
 use lightning::{
@@ -8,11 +8,10 @@ use lightning::{
         router::{build_route_from_hops, PaymentParameters, Route, RouteParameters},
     },
 };
-use rand::Rng;
 use simln_lib::{
     clock::Clock,
     sim_node::{SimGraph, SimNode, WrappedLog},
-    LightningNode,
+    LightningNode, PaymentOutcome,
 };
 use tokio::sync::Mutex;
 
@@ -43,15 +42,26 @@ pub fn build_custom_route(
     )
 }
 
-/// Helper to build reputation for a specific incoming channel (target_channel).
+pub struct BuildReputationParams<'a, C: Clock + InstantClock, R: ReputationMonitor> {
+    pub attacker_node: Arc<Mutex<SimNode<SimGraph>>>,
+    pub hops: &'a [PublicKey],
+    pub network_graph: &'a NetworkGraph<Arc<WrappedLog>>,
+    pub htlcs: Vec<u64>,
+    pub risk_margin: u64,
+    pub target_channel: (PublicKey, u64),
+    pub reputation_monitor: Arc<Mutex<R>>,
+    pub clock: Arc<C>,
+}
+
+/// Helper to build outgoing reputation towards the attacker with a specific target_channel.
 /// Arguments:
 /// - `hops`: The route for the payment that should include the outgoing channel for which we are
-/// building reputation with (do not include sender).
+///     building reputation with (do not include sender).
 /// - htlcs: Vector of htlc amounts that we'd want to be able to have in the protected bucket. E.g
-/// if [100_000] is passed, it will build enough reputation to have a 100_000 htlc in the protected
-/// bucket for the target channel.
+///     if [100_000] is passed, it will build enough reputation to have a 100_000 htlc in the protected
+///     bucket for the target channel.
 /// - target_channel: The public key and scid for the target channel that we are building
-/// reputation for.
+///     reputation for.
 ///
 /// Example: `hops` could be the following route:
 /// - A → B → C
@@ -60,20 +70,16 @@ pub fn build_custom_route(
 /// The `target_channel` is channel for which reputation is being built. So we will monitor the
 /// outgoing channel reputation (B <-> C) against the target_channel revenue.
 /// If successful, it returns the total fees paid to build reputation.
-#[allow(clippy::too_many_arguments)]
 pub async fn build_reputation<C: Clock + InstantClock, R: ReputationMonitor>(
-    attacker_node: Arc<Mutex<SimNode<SimGraph>>>,
-    hops: &[PublicKey],
-    network_graph: &NetworkGraph<Arc<WrappedLog>>,
-    htlcs: Vec<u64>,
-    risk_margin: u64,
-    target_channel: (PublicKey, u64),
-    reputation_monitor: Arc<Mutex<R>>,
-    clock: Arc<C>,
+    params: BuildReputationParams<'_, C, R>,
 ) -> Result<u64, BoxError> {
     let mut total_fees_paid = 0;
 
-    let mut attacker = attacker_node.lock().await;
+    let reputation_monitor = params.reputation_monitor;
+    let target_channel = params.target_channel;
+    let clock = params.clock;
+
+    let mut attacker = params.attacker_node.lock().await;
     let current_target_revenue = reputation_monitor
         .lock()
         .await
@@ -84,8 +90,8 @@ pub async fn build_reputation<C: Clock + InstantClock, R: ReputationMonitor>(
         .bidirectional_revenue;
 
     let attacker_pubkey = attacker.get_info().pubkey;
-    let mut route =
-        build_custom_route(&attacker_pubkey, 1000, hops, network_graph).map_err(|e| e.err)?;
+    let mut route = build_custom_route(&attacker_pubkey, 1000, params.hops, params.network_graph)
+        .map_err(|e| e.err)?;
 
     let last_hop_channel = route.paths[0]
         .hops
@@ -102,7 +108,8 @@ pub async fn build_reputation<C: Clock + InstantClock, R: ReputationMonitor>(
         .ok_or(format!("channel {} not found", last_hop_channel))?
         .outgoing_reputation;
 
-    let htlc_amounts: u64 = htlcs.iter().sum();
+    let htlc_amounts: u64 = params.htlcs.iter().sum();
+    let risk_margin = params.risk_margin;
     for path in route.paths.iter_mut() {
         total_fees_paid += path.hops.iter().map(|hop| hop.fee_msat).sum::<u64>();
 
@@ -128,7 +135,7 @@ pub async fn build_reputation<C: Clock + InstantClock, R: ReputationMonitor>(
         total_fees_paid += fee_to_bump_reputation;
     }
 
-    let payment_hash = PaymentHash(get_random_bytes());
+    let payment_hash = PaymentHash(rand::random());
     if let Err(e) = attacker
         .send_to_route(route.clone(), payment_hash, None)
         .await
@@ -136,7 +143,13 @@ pub async fn build_reputation<C: Clock + InstantClock, R: ReputationMonitor>(
         return Err(e.to_string().into());
     }
 
-    thread::sleep(Duration::from_millis(400));
+    let shutdown = triggered::trigger();
+    let payment_result = attacker.track_payment(&payment_hash, shutdown.1).await?;
+
+    match payment_result.payment_outcome {
+        PaymentOutcome::Success => {}
+        _ => return Err("payment failed to build reputation failed".into()),
+    }
 
     // After making the payment with the inflated fee, check if reputation built over outgoing
     // channel is sufficient.
@@ -163,11 +176,4 @@ pub async fn build_reputation<C: Clock + InstantClock, R: ReputationMonitor>(
     } else {
         Err("could not build reputation".into())
     }
-}
-
-pub fn get_random_bytes() -> [u8; 32] {
-    let mut rng = rand::rng();
-    let mut bytes = [0u8; 32];
-    rng.fill(&mut bytes[..]);
-    bytes
 }
